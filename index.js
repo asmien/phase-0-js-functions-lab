1b2935e74ab010fbcfa264ab70e01125e54da520
Samantha Bora <samantha.babubora@gmail.com>
2025-12-01 14:10:50 +0300

    Functions in Javascript completed

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+// Calculate Tax
+function calculateTax(amount) {
+  return amount * 0.10;
+}
 
+// Convert To UpperCase
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
 
+// Find Maximum
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
 
+// Is Palindrome
+function isPalindrome(word) {
+  const reversed = word.split("").reverse().join("");
+  return word === reversed;
+}
 
+// Calculate Discounted Price
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discountAmount = (originalPrice * discountPercentage) / 100;
+  return originalPrice - discountAmount;
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
